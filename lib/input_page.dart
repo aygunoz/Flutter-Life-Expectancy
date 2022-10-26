7e33703cff8436a72356cb8f68d3fc8442f2f8c2
Aygun Ozdemir <aygun.ozdemir@xinerji.com.tr>
2022-10-26 11:15:02 +0300

    safe area edit

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,139 +20,140 @@ class _InputPageState extends State<InputPage> {
   int kilo = 70;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'YAŞAM BEKLENTİSİ',
-          style: TextStyle(color: Colors.black54),
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: MyWidget(
-                    child: buildRowOutlineButton('BOY', boy),
-                  ),
-                ),
-                Expanded(
-                  child: MyWidget(
-                    child: buildRowOutlineButton('KİLO', kilo),
-                  ),
-                ),
-              ],
-            ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'YAŞAM BEKLENTİSİ',
+            style: TextStyle(color: Colors.black54),
           ),
-          Expanded(
-              child: MyWidget(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Haftada Kaç Kere spor yapıyorsunuz?',
-                  style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  sporGunu.round().toString(),
-                  style: TextStyle(
-                      color: Colors.lightBlue,
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold),
-                ),
-                Slider(
-                  value: sporGunu,
-                  onChanged: (double newValue) {
-                    setState(() {
-                      sporGunu = newValue;
-                    });
-                  },
-                  divisions: 7,
-                  min: 0,
-                  max: 7,
-                )
-              ],
+          centerTitle: true,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: MyWidget(
+                      child: buildRowOutlineButton('BOY', boy),
+                    ),
+                  ),
+                  Expanded(
+                    child: MyWidget(
+                      child: buildRowOutlineButton('KİLO', kilo),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )),
-          Expanded(
-            child: MyWidget(
+            Expanded(
+                child: MyWidget(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    'Günde Kaç Sigara İçiyorsunuz?',
+                    'Haftada Kaç Kere spor yapıyorsunuz?',
                     style: TextStyle(
                         color: Colors.black54,
                         fontSize: 20,
                         fontWeight: FontWeight.bold),
                   ),
                   Text(
-                    icilenSigara.round().toString(),
+                    sporGunu.round().toString(),
                     style: TextStyle(
                         color: Colors.lightBlue,
                         fontSize: 35,
                         fontWeight: FontWeight.bold),
                   ),
                   Slider(
-                    value: icilenSigara,
+                    value: sporGunu,
                     onChanged: (double newValue) {
                       setState(() {
-                        icilenSigara = newValue;
+                        sporGunu = newValue;
                       });
                     },
+                    divisions: 7,
                     min: 0,
-                    max: 30,
+                    max: 7,
                   )
                 ],
               ),
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: MyWidget(
-                    renk: seciliCinsiyet == 'KADIN'
-                        ? Colors.lightBlue[100]
-                        : Colors.white,
-                    onPress: () {
-                      setState(() {
-                        seciliCinsiyet = 'KADIN';
-                      });
-                    },
-                    child: IconCinsiyet(
-                        cinsiyet: 'KADIN', icon: FontAwesomeIcons.venus),
-                  ),
+            )),
+            Expanded(
+              child: MyWidget(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Günde Kaç Sigara İçiyorsunuz?',
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      icilenSigara.round().toString(),
+                      style: TextStyle(
+                          color: Colors.lightBlue,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Slider(
+                      value: icilenSigara,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          icilenSigara = newValue;
+                        });
+                      },
+                      min: 0,
+                      max: 30,
+                    )
+                  ],
                 ),
-                Expanded(
-                  child: MyWidget(
-                    renk: seciliCinsiyet == 'ERKEK'
-                        ? Colors.lightBlue[100]
-                        : Colors.white,
-                    onPress: () {
-                      setState(() {
-                        seciliCinsiyet = 'ERKEK';
-                      });
-                    },
-                    child: IconCinsiyet(
-                      cinsiyet: 'ERKEK',
-                      icon: FontAwesomeIcons.mars,
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MyWidget(
+                      renk: seciliCinsiyet == 'KADIN'
+                          ? Colors.lightBlue[100]
+                          : Colors.white,
+                      onPress: () {
+                        setState(() {
+                          seciliCinsiyet = 'KADIN';
+                        });
+                      },
+                      child: IconCinsiyet(
+                          cinsiyet: 'KADIN', icon: FontAwesomeIcons.venus),
                     ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: MyWidget(
+                      renk: seciliCinsiyet == 'ERKEK'
+                          ? Colors.lightBlue[100]
+                          : Colors.white,
+                      onPress: () {
+                        setState(() {
+                          seciliCinsiyet = 'ERKEK';
+                        });
+                      },
+                      child: IconCinsiyet(
+                        cinsiyet: 'ERKEK',
+                        icon: FontAwesomeIcons.mars,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          ButtonTheme(
-            height: 50,
-            child: TextButton(
+            ButtonTheme(
+              height: 50,
+              child: TextButton(
                 style: TextButton.styleFrom(backgroundColor: Colors.white),
                 onPressed: () {
                   Navigator.push(
@@ -171,9 +172,11 @@ class _InputPageState extends State<InputPage> {
                       color: Colors.black54,
                       fontSize: 24,
                       fontWeight: FontWeight.bold),
-                )),
-          )
-        ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
